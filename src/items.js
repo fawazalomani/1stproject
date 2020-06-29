cfab3eb02a56a310fae698c47ec237a40fd32c70
fawaz <st0ock@hotmail.com>
2020-06-30 00:22:33 +0300

    added map

---
+++ src/items.js
@@ -0,0 +1,26 @@
+const produts = [
+  {
+    id: 1,
+    name: "tool Box",
+    price: "36 KD",
+    image:
+      "https://static01.nyt.com/images/2017/05/21/realestate/21FIX1/21FIX1-superJumbo.jpg",
+  },
+  {
+    id: 2,
+    name: "Phones",
+    price: "21 KD",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/61wjAvw5B2L._AC_SX425_.jpg",
+  },
+
+  {
+    id: 3,
+    name: "Computers",
+    price: "210 KD",
+    image:
+      "https://img.etimg.com/thumb/width-640,height-480,imgsize-290552,resizemode-1,msid-75719903/traditional-personal-computer-shipments-fall-by-17-in-first-quarter-idc.jpg",
+  },
+];
+
+export default produts;
---
+++ src/styles.js
@@ -0,0 +1,28 @@
+const styles = {
+  text: {
+    textAlign: "center",
+  },
+
+  shopimage: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+
+  prodimag: {
+    width: "200px",
+    height: "200px",
+  },
+
+  list: {
+    alignitems: "center",
+    justifyContent: "center",
+    display: "flex",
+  },
+
+  photmar: {
+    margin: "30px",
+  },
+};
+
+export default styles;
